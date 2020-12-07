fc240a60d0a3d3e9ac8babfdb432c50c33eeadca
chen guangtao <39899119+792607724@users.noreply.github.com>
2020-12-07 22:18:10 +0800

    Case add

--- case_construct.py
+++ case_construct.py
@@ -1,15 +1,16 @@
 # coding = utf8
-import os
+from time import sleep
+
 from airtest.core.api import *
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
-from time import sleep
+
 os.path.abspath(".")
 
 poco = AndroidUiautomationPoco()
 austinDevice = connect_device("Android:///7c2440fd")
 
-
 # 将case进行分解->剔出通用common方法->将APP分页处理->case整合->架构完善
+# 状态更改需要先进行判断设置为除自身外其它状态,如默认checked = true，则设置为false，并记录前后状态以及Fota后状态
 
 """
     Case 1:获取本地应用程序版本被保留
@@ -18,11 +19,14 @@ austinDevice = connect_device("Android:///7c2440fd")
     test step：
         检查APP存在->获取APP版本并保存->Fota升级后再次获取版本号与升级前对比是否相同判定结果
 """
-def get_app_version(packageName = "com.android.settings"):
+
+
+def get_app_version(packageName="com.android.settings"):
     exists_app = austinDevice.check_app(packageName)
     if exists_app:
-        versionName = austinDevice.shell("pm dump %s|grep versionName" %packageName)
+        versionName = austinDevice.shell("pm dump %s|grep versionName" % packageName)
         print(versionName)
+
 
 """
     Case 2:通话配置修改被保留
@@ -31,8 +35,11 @@ def get_app_version(packageName = "com.android.settings"):
     test step:
         检查APP存在->进入Dialer->点击菜单->Settings->Display options->Sort by->更改值并保存->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
+
+
 def change_calling_settings():
     start_app("com.google.android.dialer")
+
 
 """
     Case 3:验证通话记录被保留
@@ -41,9 +48,12 @@ def change_calling_settings():
     test step:
         检查APP存在->通过adb拨打电话->进入APP->Recents->保存Top 1通话记录->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
-def check_calling_history(number = "10086"):
+
+
+def check_calling_history(number="10086"):
     start_app("com.google.android.dialer")
-    austinDevice.shell("am start -a android.intent.action.CALL tel:%s" %number)
+    austinDevice.shell("am start -a android.intent.action.CALL tel:%s" % number)
+
 
 """
     Case 4:验证短信已发送、接受、草稿被保留
@@ -52,29 +62,36 @@ def check_calling_history(number = "10086"):
     test step:
         检查APP存在->通过adb发送短信给自身号码->检查是否发送成功并保存结果->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
-def check_sms_sender_receiver(number = "18575211714", content = "Test"):
+
+
+def check_sms_sender_receiver(number="18575211714", content="Test"):
     start_app("com.google.android.apps.messaging")
-    austinDevice.shell("am start -a android.intent.action.SENDTO -d sms:%s --es sms_body %s" %(number, content))
-    poco(text = "SMS").click()
+    austinDevice.shell("am start -a android.intent.action.SENDTO -d sms:%s --es sms_body %s" % (number, content))
+    poco(text="SMS").click()
     sleep(3)
     receiver_content = poco("android:id/list").children()[2].get_name()
     print(receiver_content)
     if number[-4:] in receiver_content:
         print("PASS")
 
+
 """
     Case 5:验证短信相关设置是否被保留
     relate app:
         com.google.android.apps.messaging
     test step:
-        检查APP存在->点击menu->Settings->Disable "Hear outgoing message sounds"->记录当前状态->Fota升级后再次获取该值与升级前对比是否相同判定结果
+        检查APP存在->点击menu->Settings->Disable "Hear outgoing message sounds"->记录默认、修改、升级后元素状态(invalidate进行元素刷新)->
+        Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
+
+
 def change_message_settings():
     stop_app("com.google.android.apps.messaging")
     sleep(1)
     start_app("com.google.android.apps.messaging")
     switch_on_off = poco('com.google.android.apps.messaging:id/switchWidget')
     print(switch_on_off.attr("checked"))
+
 
 """
     Case 6:验证手机联系人是否被保留
@@ -83,9 +100,12 @@ def change_message_settings():
     test step:
         检查APP存在->adb创建联系人->Save->记录当前联系人->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
-def check_contact_reserved(contact_name = "Test", phone_number = "18575211714"):
+
+
+def check_contact_reserved(contact_name="Test", phone_number="18575211714"):
     austinDevice.shell("am start -a android.intent.action.INSERT -t vnd.android.cursor.dir/"
-                       "contact -e name %s -e phone %s" %(contact_name, phone_number))
+                       "contact -e name %s -e phone %s" % (contact_name, phone_number))
+
 
 """
     Case 7:验证创建的Clock是否被保留
@@ -94,8 +114,11 @@ def check_contact_reserved(contact_name = "Test", phone_number = "18575211714"):
     test step:
         检查APP存在->点击"+"->记录Hour + Minutes->点击保存闹钟->在列表界面check闹钟创建成功->记录闹钟时间->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
+
+
 def check_clock_reserved():
     start_app("com.android.deskclock")
+
 
 """
     Case 8:验证创建的Calendar是否被保留
@@ -104,19 +127,79 @@ def check_clock_reserved():
     test step:
         检查APP存在->跳过Guide->点击"+"->输入Test->点击Save->记录Title->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
+
+
 def check_calendar_reserved():
     start_app("com.google.android.calendar")
+
 
 """
     Case 9:验证Battery management设置是否被保留
     relate app:
         com.tct.onetouchbooster
     test step:
-        检查APP存在->点击x关闭guide->点击Battery->点击Settings->Disable "Intelligent power saving"->记录当前状态->Fota升级后再次获取该值与升级前对比是否相同判定结果
+        检查APP存在->点击x关闭guide->点击Battery->点击Settings->Disable "Intelligent power saving"
+        ->记录默认、修改、升级后元素状态(invalidate进行元素刷新)->Fota升级后再次获取该值与升级前对比是否相同判定结果
 """
+
+
 def change_battery_management_settings():
     start_app("com.tct.onetouchbooster")
 
 
+"""
+    Case 10:验证相机设置是否被保留
+    relate app:
+        com.tcl.camera
+        com.google.android.permissioncontroller
+    test step:
+        检查APP存在->点击WHILE USING THE APP进行授权->点击Settings->点击->点击AI scene detection更改状态
+        ->记录默认、修改、升级后元素状态(invalidate进行元素刷新)->Fota升级后再次获取该值与升级前对比是否相同判定结果
+"""
+
+
+def change_camera_settings():
+    start_app("com.tcl.camera")
+    sleep(1)
+    camera_settings = (poco("com.tcl.camera:id/picker_list_layout").children()[0]).wait()
+    sleep(1)
+    camera_settings.click()
+    sleep(1)
+    print(poco(text="AI scene detection").parent().parent().children()[1].children().attr("checked"))
+
+
+"""
+    Case 11:验证声音设置是否被保留
+    relate app:
+        com.android.settings
+    test step:
+        检查APP存在->Settings->Sound & vibration->Enable Silent mode->记录默认、修改、升级后元素状态(invalidate进行元素刷新)
+        ->Fota升级后再次获取该值与升级前对比是否相同判定结果
+"""
+
+
+def change_sound_settings():
+    apps_menu = None
+    start_app("com.android.settings")
+    menu_exists = False
+    while not menu_exists:
+        apps_menu = poco(text="Sound & vibration").wait()
+        menu_exists = apps_menu.exists()
+        if menu_exists:
+            break
+        poco.scroll(direction="vertical", percent=0.8, duration=1)
+    apps_menu.click()
+    silent_mode_switch = poco(text="Silent mode").parent().parent().children()[1].child(
+        "android:id/switch_widget").wait()
+    checked_result_default = silent_mode_switch.attr("checked")
+    silent_mode_switch.click()
+    # 清除该标志以重新获取该元素
+    silent_mode_switch.invalidate()
+    checked_result_changed = silent_mode_switch.attr("checked")
+    checked_result_updated = silent_mode_switch.wait().attr("checked")
+    print(str(checked_result_default) + "-" + str(checked_result_changed) + "-" + str(checked_result_updated))
+
+
 if __name__ == "__main__":
+    wake()
     home()
